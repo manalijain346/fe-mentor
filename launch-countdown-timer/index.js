dbec1f874ef4e67018dc9cfd3a52541e4c381a95
Manali Jain <manalijain346@gmail.com>
2022-01-12 21:37:02 +0530

    enhancement

--- launch-countdown-timer/index.js
+++ launch-countdown-timer/index.js
@@ -1,22 +1,32 @@
-var Countdown= new Date("Feb 15,2022 00:00:00").getTime();
-var x=setInterval (function(){
- var now=new Date().getTime();
-  var timeLeft=Countdown-now;
-    var days = Math.floor( timeLeft/(1000*60*60*24) );
-    var hours = Math.floor( (timeLeft/(1000*60*60)) % 24 );
-    var minutes = Math.floor( (timeLeft/1000/60) % 60 );
-    var seconds = Math.floor( (timeLeft/1000) % 60 );
+var daysLabel = document.getElementById("days");
+var hoursLabel = document.getElementById("hours");
+var minutesLabel = document.getElementById("minutes");
+var secondsLabel = document.getElementById("seconds");
 
-    hours= hours <10 ? "0"+ hours : hours
+var Countdown = new Date(`Feb 15,2022, 00:00:00`).getTime();
 
-    document.getElementById("days").innerHTML= days
-    document.getElementById("hours").innerHTML= hours
-    document.getElementById("minutes").innerHTML= minutes
-    document.getElementById("seconds").innerHTML= seconds
-
-    if (timeLeft < 0) {
+var x = setInterval(function () {
+    var now = new Date().getTime();
+    var timeLeft = Countdown - now;
+    if (timeLeft <= 0) {
         clearInterval(x);
         console.log('CountDown Finished');
+        return;
     }
-},)
+    var days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((timeLeft / (1000 * 60 * 60)) % 24);
+    var minutes = Math.floor((timeLeft / 1000 / 60) % 60);
+    var seconds = Math.floor((timeLeft / 1000) % 60);
 
+    days = days < 10 ? "0" + days : days
+    hours = hours < 10 ? "0" + hours : hours
+    minutes = minutes < 10 ? "0" + minutes : minutes
+    seconds = seconds < 10 ? "0" + seconds : seconds
+
+    daysLabel.innerHTML = days
+    hoursLabel.innerHTML = hours
+    minutesLabel.innerHTML = minutes
+    secondsLabel.innerHTML = seconds
+
+    
+}, )
